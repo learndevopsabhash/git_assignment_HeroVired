255e3a1984ae120b1071d3d6974e85dfd3d46af5
AbhashJain <learndevopsabhash@outlook.com>
2026-03-22 23:12:33 +0530

    Version 2

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -15,7 +15,7 @@ class Calculator:
             raise ValueError("Cannot divide by zero.")
         return a / b
 
-# TODO: Implement the following function to calculate the square root of a number.
+# TODO.: Implement the following function to calculate the square root of a number.
 
     def square_root(self, x):
         return math.sqrt(x)
